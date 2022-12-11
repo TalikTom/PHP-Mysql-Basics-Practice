2b0ceca9b6b48c49e0376df2a6b4d886ff5952a6
Luka Agic <lukagic@gmail.com>
2022-12-11 23:12:04 +0100

    Add table

--- index.php
+++ index.php
@@ -22,5 +22,23 @@ for ($i = 1; $i < 10; $i++) {
 <p><?= $greeting ?></p>
 <h2><?= $product ?> Discounts</h2>
 
+<table>
+    <tr>
+        <th>Packs</th>
+        <th>Price</th>
+    </tr>
+    <?php foreach ($totals as $quantity => $price) { ?>
+        <tr>
+            <td>
+                <?= $quantity ?>
+                pack<?= ($quantity === 1) ? '' : 's' ?>
+            </td>
+            <td>
+                $<?= $price ?>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
+
 
 <?php require 'includes/footer.php'; ?>
